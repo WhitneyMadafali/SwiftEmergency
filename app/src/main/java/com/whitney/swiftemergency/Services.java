0c6c3cb6f71d34c507ca4bfa64961d63ee0843aa
Vincent Mutungi <Vincent.Mutungi@O365.strathmore.edu>
2021-08-05 11:47:42 +0300

    Removed unnecessary services

--- app/src/main/java/com/whitney/swiftemergency/Services.java
+++ app/src/main/java/com/whitney/swiftemergency/Services.java
@@ -11,8 +11,8 @@ import android.view.View;
 
 public class Services extends AppCompatActivity implements View.OnClickListener {
 
-    public CardView card1, card2, card3, card4, card5, card6;
-    String name1, name2, name3, name4, name5, name6;
+    public CardView card1, card2, card3, card4;
+    String name1, name2, name3, name4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +22,17 @@ public class Services extends AppCompatActivity implements View.OnClickListener 
         card1 = (CardView) findViewById(R.id.FIRE);
         card2 = (CardView) findViewById(R.id.POLICE);
         card3 = (CardView) findViewById(R.id.AMBULANCE);
-        card4 = (CardView) findViewById(R.id.ROAD_ACCIDENT);
-        card5 = (CardView) findViewById(R.id.FLOODS);
-        card6 = (CardView) findViewById(R.id.VIOLENCE);
+        card4 = (CardView) findViewById(R.id.VIOLENCE);
 
         name1 = "FIRE";
         name2 = "POLICE";
         name3 = "AMBULANCE";
-        name4 = "ROAD ACCIDENT";
-        name5 = "FLOODS";
-        name6 = "VIOLENCE";
+        name4 = "VIOLENCE";
 
         card1.setOnClickListener(this);
         card2.setOnClickListener(this);
         card3.setOnClickListener(this);
         card4.setOnClickListener(this);
-        card5.setOnClickListener(this);
-        card6.setOnClickListener(this);
     }
 
     @SuppressLint("NonConstantResourceId")
