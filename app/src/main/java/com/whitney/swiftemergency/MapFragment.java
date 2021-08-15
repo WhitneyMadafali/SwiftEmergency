7c263924052ff9043b75b01d6e92754da236fb7a
Vincent Mutungi <Vincent.Mutungi@O365.strathmore.edu>
2021-08-15 18:10:39 +0300

    Markers Updated

--- app/src/main/java/com/whitney/swiftemergency/MapFragment.java
+++ app/src/main/java/com/whitney/swiftemergency/MapFragment.java
@@ -26,6 +26,11 @@ public class MapFragment extends Fragment {
     ArrayList<LatLng>arrayList = new ArrayList<LatLng>();
     LatLng hospital1 = new LatLng(-1.309449,  36.814782);
     LatLng hospital2 = new LatLng(-1.3079015, 36.8033858);
+    LatLng police1 = new LatLng(-1.303275,  36.806269);
+    LatLng police2 = new LatLng(-1.304958, 36.825213);
+    LatLng fire1 = new LatLng(-1.287635,  36.813613);
+    LatLng fire2 = new LatLng(-1.281757, 36.823121);
+    LatLng violence1 = new LatLng(-1.300707,  36.806465);
     // arraylist for names of markers
     ArrayList<String> title = new ArrayList<String>();
 
@@ -51,10 +56,21 @@ public class MapFragment extends Fragment {
                 //when map is loaded
                 arrayList.add(hospital1);
                 arrayList.add(hospital2);
+                arrayList.add(police1);
+                arrayList.add(police2);
+                arrayList.add(fire1);
+                arrayList.add(fire2);
+                arrayList.add(violence1);
                 // adding titles
 
                 title.add("Strathmore Clinic");
                 title.add("Mbaghathi Hospital");
+                title.add("K.N.H Police Station");
+                title.add("Nyayo Police Station");
+                title.add("G4S Fire Service");
+                title.add("Nairobi County Fire Station");
+                title.add("Gender Based Violence Recovery Centre K.N.H");
+
 
                 for (int i=0;i<arrayList.size();i++){
                     //for adding markers
